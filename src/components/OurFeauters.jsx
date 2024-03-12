1145c7f579fd980754fa9dd8915ae5a51a5d709c
AAYUSH BISHNOI <aayushkaswan79@gmail.com>
2024-03-12 15:08:14 +0530

    card-shadow

--- src/components/OurFeauters.jsx
+++ src/components/OurFeauters.jsx
@@ -16,7 +16,8 @@ const OurFeauters = () => {
           </p>
         </div>
         <div className="flex flex-wrap justify-center items-center lg:justify-between md:gap-5 lg:gap-0 mt-5 md:mt-10">
-            <div className="bg-white border border-black hover:shadow-[0px_3px_0px_0px#000000] w-[360px] lg:w-[320px] xl:w-[360px] h-[238px] rounded-2xl duration-300 ease-in-out flex justify-center items-center flex-col">
+          <div className="border border-black rounded-2xl">
+            <div className="bg-white hover:shadow-[0px_4px_0px_0px#000000] w-[360px] lg:w-[320px] xl:w-[360px] h-[238px] rounded-2xl duration-300 ease-in-out flex justify-center items-center flex-col">
               <div className=" relative">
                 <Bgelipse />
                 <Image
@@ -30,33 +31,38 @@ const OurFeauters = () => {
                 Easy To Order
               </p>
             </div>
-          <div className="bg-white border border-black hover:shadow-[0px_3px_0px_0px#000000] w-[360px] lg:w-[320px] xl:w-[360px] h-[238px] rounded-2xl duration-300 ease-in-out flex justify-center items-center flex-col mt-4 md:mt-0">
-            <div className=" relative">
-              <Bgelipse />
-              <Image
-                src="/assets/images/Our/Svg/Truck.svg"
-                width={100}
-                height={100}
-                className="absolute -translate-x-[50%] -translate-y-[50%] top-[50%] left-[50%]"
-              />
-            </div>
-            <p className=" font-ClashDisplay font-medium text-lg !leading-[123%] text-[#0C0800] mt-3">
-              Fastest Delivery
-            </p>
           </div>
-          <div className="bg-white border border-black hover:shadow-[0px_3px_0px_0px#000000] w-[360px] lg:w-[320px] xl:w-[360px] h-[238px] rounded-2xl duration-300 ease-in-out flex justify-center items-center flex-col mt-4 md:mt-0">
-            <div className=" relative">
-              <Bgelipse />
-              <Image
-                src="/assets/images/Our/Svg/Medal.svg"
-                width={100}
-                height={100}
-                className="absolute -translate-x-[50%] -translate-y-[50%] top-[50%] left-[50%]"
-              />
+          <div className="border border-black rounded-2xl">
+            <div className="bg-white hover:shadow-[0px_4px_0px_0px#000000] w-[360px] lg:w-[320px] xl:w-[360px] h-[238px] rounded-2xl duration-300 ease-in-out flex justify-center items-center flex-col mt-4 md:mt-0">
+              <div className=" relative">
+                <Bgelipse />
+                <Image
+                  src="/assets/images/Our/Svg/Truck.svg"
+                  width={100}
+                  height={100}
+                  className="absolute -translate-x-[50%] -translate-y-[50%] top-[50%] left-[50%]"
+                />
+              </div>
+              <p className=" font-ClashDisplay font-medium text-lg !leading-[123%] text-[#0C0800] mt-3">
+                Fastest Delivery
+              </p>
             </div>
-            <p className=" font-ClashDisplay font-medium text-lg !leading-[123%] text-[#0C0800] mt-3">
-              Best Quality
-            </p>
+          </div>
+          <div className="border border-black rounded-2xl">
+            <div className="bg-white hover:shadow-[0px_4px_0px_0px#000000] w-[360px] lg:w-[320px] xl:w-[360px] h-[238px] rounded-2xl duration-300 ease-in-out flex justify-center items-center flex-col mt-4 md:mt-0">
+              <div className=" relative">
+                <Bgelipse />
+                <Image
+                  src="/assets/images/Our/Svg/Medal.svg"
+                  width={100}
+                  height={100}
+                  className="absolute -translate-x-[50%] -translate-y-[50%] top-[50%] left-[50%]"
+                />
+              </div>
+              <p className=" font-ClashDisplay font-medium text-lg !leading-[123%] text-[#0C0800] mt-3">
+                Best Quality
+              </p>
+            </div>
           </div>
         </div>
       </div>
