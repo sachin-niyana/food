e6990939f92e0c281b4b30f6800f74b9d3c1598e
Naveen <naveenninu888@gmail.com>
2024-03-12 11:29:33 +0530

    add font

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,4 @@
+import Demo from "@/components/Demo";
 import Navbar from "@/components/Navbar";
 import OurChef from "@/components/OurChef";
 
@@ -5,6 +6,7 @@ export default function Home() {
   return (
     <div>
       <Navbar />
+      <Demo />
     </div>
   );
 }
---
+++ src/components/Demo.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Demo() {
+    return (
+        <div className='font-ClashDisplay text-center text-3xl font-semibold'>Our Features</div>
+    )
+}
+
+export default Demo
