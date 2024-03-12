969a7d69b462ca6c6e392d7e112b3c734e0503e4
sachin-niyana <sachinjakhar100@gmail.com>
2024-03-12 11:16:25 +0530

    marge-code:

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,11 @@
+import Header from "@/components/Header";
 import Navbar from "@/components/Navbar";
-import OurChef from "@/components/OurChef";
 
 export default function Home() {
   return (
     <div>
       <Navbar />
+      <Header />
     </div>
   );
 }
