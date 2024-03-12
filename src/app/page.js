d2a11b4e90c5b39e64ea39b77c255f0c1a3cda48
jatin arya <89856685+jatinarya21@users.noreply.github.com>
2024-03-12 11:17:10 +0530

    our chef section complete

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,13 @@
+import FooterSection from "@/components/FooterSection";
 import Navbar from "@/components/Navbar";
 import OurChef from "@/components/OurChef";
 
 export default function Home() {
   return (
     <div>
-      {/* <Navbar /> */}
+      <Navbar />
       <OurChef />
+      <FooterSection />
     </div>
   );
 }
---
+++ src/components/FooterSection.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const FooterSection = () => {
+    return (
+        <div>
+            <div className='container mx-auto max-w-[1152px] px-3'>
+
+            </div>
+        </div>
+    )
+}
+
+export default FooterSection
