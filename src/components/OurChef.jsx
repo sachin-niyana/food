c8914fd736c82489bf0f2294d0679c5da9826de8
kulvinder02 <kulvinderjangra02@gmail.com>
2024-03-15 14:58:33 +0530

    done

--- src/components/OurChef.jsx
+++ src/components/OurChef.jsx
@@ -13,7 +13,7 @@ const OurChef = () => {
       id="Contact"
     >
       <div className="flex flex-col md:flex-row items-center md:justify-between pb-10 md:pb-4 lg:pb-10">
-        <div className="w-11/12 sm:w-8/12 md:w-9/12 lg:w-6/12">
+        <div className="w-full sm:w-8/12 md:w-9/12 lg:w-6/12">
           <h1
             className="font-ClashDisplay text-center md:text-start lg:text-2xl md:text-xl text-lg text-Rich-Black font-semibold"
             data-aos="fade-right"
@@ -23,7 +23,7 @@ const OurChef = () => {
             Our Chef
           </h1>
           <p
-            className="font-Syne px-3 sm:px-0 text-xsm sm:text-sm font-normal text-Rich-Black text-center md:text-start opacity-70"
+            className="font-Syne px-3 sm:px-0 text-sm font-normal text-Rich-Black text-center md:text-start opacity-70"
             data-aos="fade-right"
             data-aos-easing="linear"
             data-aos-duration="800"
@@ -38,16 +38,52 @@ const OurChef = () => {
           data-aos-easing="linear"
           data-aos-duration="800"
         >
-          <button className="bg-linear-gradient-btn hover:shadow-buttonShadow transition-all ease-in-out duration-300 flex items-center font-ClashDisplay text-sm font-medium px-5 py-3 rounded-full gap-1.5">
-            <RightArrow /> See All
-          </button>
+          <div className=" mt-[30px] sm:mt-[60px] mb-[82px] hidden lg:block  lg:mt-0">
+            <ShowMoreBtn
+              data={
+                <div className="sm:hidden lg:block">
+                  <div className="flex flex-wrap lg:grid grid-cols-3 gap-6 items-center justify-center">
+                    {ourChef.slice(1, 3).map((value, index) => (
+                      <div
+                        data-aos="zoom-in"
+                        data-aos-easing="linear"
+                        data-aos-duration="800"
+                        key={index}
+                      >
+                        <div className="rounded-2xl p-5 border group bg-white max-w-[360px]  border-black">
+                          <div className="max-w-[310px] max-h-[310px] overflow-hidden rounded-2xl">
+                            <Image
+                              className="rounded-2xl group-hover:scale-[1.04]  duration-300"
+                              width={316}
+                              height={315}
+                              src={value.image}
+                              alt="john-doe-chef-master"
+                            />
+                          </div>
+                          <div className="flex justify-between lg:pr-4 items-center pt-[18px] ">
+                            <div>
+                              <h3 className="lg:text-lg md:text-md text-sm font-medium font-ClashDisplay text-Rich-Black">
+                                {value.title}
+                              </h3>
+                              <p className="text-Rich-Black opacity-70 font-Syne font-normal text-sm">
+                                {value.master}
+                              </p>
+                            </div>
+                            <Link href="https://www.linkedin.com/" target="_blank">
+                              {value.icon}
+                            </Link>
+                          </div>
+                        </div>
+                      </div>
+                    ))}
+                  </div>
+                </div>
+              }
+            />
+          </div>
         </div>
       </div>
 
-
-      <div className="hidden sm:block lg:hidden">
-        <MarqueeSlider />
-      </div>
 
 
       {/* MD SCREEN CARDS  */}
@@ -91,8 +127,8 @@ const OurChef = () => {
       </div>
 
       {/* SM SHOW MORE BTN CARDS  */}
-      <div className="sm:hidden">
-        <div className="flex flex-wrap lg:grid grid-cols-3 gap-6 items-center justify-center">
+      <div className="md:hidden">
+        <div className="flex flex-wrap lg:grid grid-cols-3  gap-6 items-center justify-center">
           {ourChef.slice(0, 1, 2).map((value, index) => (
             <div
               data-aos="zoom-in"
@@ -129,12 +165,53 @@ const OurChef = () => {
         </div>
       </div>
 
-      <div className="flex justify-center items-center mt-[30px] sm:mt-[60px] mb-[82px] md:hidden  lg:mt-0">
+
+      {/* MD SHOW MORE BTN CARDS  */}
+
+      <div className="hidden md:block mt-6 lg:hidden">
+        <div className="flex flex-wrap lg:grid grid-cols-3 gap-6 items-center justify-center">
+          {ourChef.slice(0, 2).map((value, index) => (
+            <div
+              data-aos="zoom-in"
+              data-aos-easing="linear"
+              data-aos-duration="800"
+              key={index}
+            >
+              <div className="rounded-2xl p-5 border group bg-white max-w-[360px]  border-black">
+                <div className="max-w-[310px] max-h-[310px] overflow-hidden rounded-2xl">
+                  <Image
+                    className="rounded-2xl group-hover:scale-[1.04]  duration-300"
+                    width={316}
+                    height={315}
+                    src={value.image}
+                    alt="john-doe-chef-master"
+                  />
+                </div>
+                <div className="flex justify-between lg:pr-4 items-center pt-[18px] ">
+                  <div>
+                    <h3 className="lg:text-lg md:text-md text-sm font-medium font-ClashDisplay text-Rich-Black">
+                      {value.title}
+                    </h3>
+                    <p className="text-Rich-Black opacity-70 font-Syne font-normal text-sm">
+                      {value.master}
+                    </p>
+                  </div>
+                  <Link href="https://www.linkedin.com/" target="_blank">
+                    {value.icon}
+                  </Link>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+
+      <div className="flex justify-center items-center mt-[30px] sm:mt-[60px] mb-[82px] md:mt-6 md:mb-10 lg:hidden  lg:mt-0">
         <ShowMoreBtn
           data={
-            <div className="sm:hidden lg:block">
-              <div className="flex flex-wrap lg:grid grid-cols-3 gap-6 items-center justify-center">
-                {ourChef.slice(1, 3).map((value, index) => (
+            <div className="lg:hidden">
+              <div className="flex flex-wrap lg:grid grid-cols-3 gap-6  items-center justify-center">
+                {ourChef.slice0(1 ? 1 : 2).map((value, index) => (
                   <div
                     data-aos="zoom-in"
                     data-aos-easing="linear"
@@ -167,11 +244,13 @@ const OurChef = () => {
                     </div>
                   </div>
                 ))}
+
               </div>
             </div>
           }
         />
       </div>
+
 
     </div>
   );
