cb36b33147136146a5fbaa4823416c2c138ed694
kulvinder02 <kulvinderjangra02@gmail.com>
2024-03-15 16:05:05 +0530

    done

--- src/components/OurChef.jsx
+++ src/components/OurChef.jsx
@@ -1,12 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import Image from "next/image";
 import { ourChef } from "./Helper";
 import { RightArrow } from "./Icon";
 import Link from "next/link";
-import MarqueeSlider from "./MarqueeSlider";
 import ShowMoreBtn from "./ShowMoreBtn";
 
 const OurChef = () => {
+  const [showOneCard, setShowOneCard] = useState(false);
   return (
     <div
       className="container max-w-[1152px] mx-auto px-3 lg:py-[130px] md:py-20 sm:py-16 py-12"
@@ -84,10 +84,7 @@ const OurChef = () => {
         </div>
       </div>
 
-
-
       {/* MD SCREEN CARDS  */}
-
       <div className="hidden lg:block">
         <div className="flex flex-wrap lg:grid grid-cols-3 gap-6 items-center justify-center">
           {ourChef.map((value, index) => (
@@ -211,13 +208,12 @@ const OurChef = () => {
           data={
             <div className="lg:hidden">
               <div className="flex flex-wrap lg:grid grid-cols-3 gap-6  items-center justify-center">
-                {ourChef.slice0(1 ? 1 : 2).map((value, index) => (
+                {ourChef.slice(0, showOneCard ? 1 : 2).map((value, index) => (
                   <div
                     data-aos="zoom-in"
                     data-aos-easing="linear"
                     data-aos-duration="800"
-                    key={index}
-                  >
+                    key={index} >
                     <div className="rounded-2xl p-5 border group bg-white max-w-[360px]  border-black">
                       <div className="max-w-[310px] max-h-[310px] overflow-hidden rounded-2xl">
                         <Image
