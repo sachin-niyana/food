b7496e0e979a4d9b200f7c6a39589be577509897
sachin-niyana <sachinjakhar100@gmail.com>
2024-03-16 09:37:13 +0530

    add meta tag

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,17 +1,29 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Food",
   description: "WELCOME TO FOOD PAGE",
+  imageUrl: "assets/images/meta/meta-img.webp",
 };
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en" className="bg-light-white scroll-smooth">
-      <body className={inter.className}>{children}</body>
-    </html>
+    <>
+      <Head>
+        <title>{metadata.title}</title>
+        <meta name="description" content={metadata.description} />
+        <meta property="og:title" content={metadata.title} />
+        <meta property="og:description" content={metadata.description} />
+        <meta property="og:image" content={metadata.imageUrl} />
+        {/* Add more meta tags as needed */}
+      </Head>
+      <html lang="en" className="bg-light-white scroll-smooth">
+        <body className={inter.className}>{children}</body>
+      </html>
+    </>
   );
 }
