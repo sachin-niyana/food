9561330e5ae9caf918564a867712afae3313e8d4
sachin-niyana <sachinjakhar100@gmail.com>
2024-03-16 10:29:22 +0530

    add meta-img

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,28 +1,22 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
+  metadataBase: "https://food-phi-six.vercel.app/",
   title: "Food",
-  description: "Welcome To Food Page",
-  imageUrl: "/public/assets/images/meta/meta-img.webp",
-  siteUrl: "https://food-phi-six.vercel.app/",
+  description: "Welcome To Food Page ...",
+  openGraph: {
+    title: "Food",
+    description: "Welcome To Food Page ...",
+    images: ["/assets/images/meta/meta-img.webp"],
+  },
 };
 
 export default function RootLayout({ children }) {
   return (
     <>
-      <Head>
-        <title>{metadata.title}</title>
-        <meta name="description" content={metadata.description} />
-        <meta property="og:title" content={metadata.title} />
-        <meta property="og:description" content={metadata.description} />
-        <meta property="og:image" content={metadata.imageUrl} />
-        <meta property="og:url" content={metadata.siteUrl} />
-        {/* Add more meta tags as needed */}
-      </Head>
       <html lang="en" className="bg-light-white scroll-smooth">
         <body className={inter.className}>{children}</body>
       </html>
