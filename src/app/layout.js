b93e4df2ad61ca16d245b176b10c47a7bc3f08eb
sachin-niyana <sachinjakhar100@gmail.com>
2024-03-16 09:47:48 +0530

    change meta link

--- src/app/layout.js
+++ src/app/layout.js
@@ -8,6 +8,7 @@ export const metadata = {
   title: "Food",
   description: "Welcome To Food Page",
   imageUrl: "/public/assets/images/meta/meta-img.webp",
+  siteUrl: "https://food-phi-six.vercel.app/",
 };
 
 export default function RootLayout({ children }) {
@@ -19,6 +20,7 @@ export default function RootLayout({ children }) {
         <meta property="og:title" content={metadata.title} />
         <meta property="og:description" content={metadata.description} />
         <meta property="og:image" content={metadata.imageUrl} />
+        <meta property="og:url" content={metadata.siteUrl} />
         {/* Add more meta tags as needed */}
       </Head>
       <html lang="en" className="bg-light-white scroll-smooth">
